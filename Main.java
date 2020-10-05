c4807db1c34275b2b946517c0a40f6f7e785436b
Marco Túlio <marcotmotta@gmail.com>
2020-10-05 09:10:52 -0300

    commit 2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args){
     Testes tests = new Testes();
+    tests.testEquality();
     tests.testMultiplication();
   }
 }
--- Money.java
+++ Money.java
@@ -1,5 +1,29 @@
-class Dollar {
-  int amount = 10;
-  Dollar(int amount) {}			
-  void times(int multiplier) {}
+class Money  {
+   protected int amount;
+
+   public boolean equals(Object object)  {
+      Money money = (Money) object;
+      return this.amount == money.amount;
+   }
+}
+
+class Dollar extends Money {
+  Dollar(int amount) {
+    this.amount = amount;  
+  }
+
+  Dollar times(int multiplier) {
+    return new Dollar(this.amount * multiplier);
+  }
+
+}
+
+class Franc extends Money {
+  Franc(int amount) {
+    this.amount = amount;
+  }
+
+  Franc times(int multiplier)  {
+    return new Franc(this.amount * multiplier);
+  }
 }
